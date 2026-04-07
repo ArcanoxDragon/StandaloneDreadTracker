16ebd27f200cf0aaa0041d6284ec09caf2b7828c
Arcanox <arcanox@arcanox.me>
2026-04-06 20:31:31 -0500

    Fix window styling on Windows 10 (and make Mica styles reusable)

---
+++ StandaloneDreadTracker.UI.Avalonia/StandaloneDreadTracker.UI.Avalonia/Utilities/WindowHelpers.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace StandaloneDreadTracker.UI.Avalonia.Utilities;
+
+public static class WindowHelpers
+{
+	static WindowHelpers()
+	{
+		TopLevel.ActualTransparencyLevelProperty.Changed.AddClassHandler<TopLevel>(OnActualTransparencyLevelChanged);
+	}
+
+	public static readonly AttachedProperty<bool> IsUsingMicaProperty        = AvaloniaProperty.RegisterAttached<TopLevel, bool>("IsUsingMica", typeof(WindowHelpers));
+	public static readonly AttachedProperty<bool> IsUsingAcrylicBlurProperty = AvaloniaProperty.RegisterAttached<TopLevel, bool>("IsUsingAcrylicBlur", typeof(WindowHelpers));
+
+	public static bool GetIsUsingMica(TopLevel target)
+		=> target.GetValue(IsUsingMicaProperty);
+
+	public static bool GetIsUsingAcrylicBlur(TopLevel target)
+		=> target.GetValue(IsUsingAcrylicBlurProperty);
+
+	private static void OnActualTransparencyLevelChanged(TopLevel target, AvaloniaPropertyChangedEventArgs args)
+	{
+		target.SetValue(IsUsingMicaProperty, target.ActualTransparencyLevel == WindowTransparencyLevel.Mica);
+		target.SetValue(IsUsingAcrylicBlurProperty, target.ActualTransparencyLevel == WindowTransparencyLevel.AcrylicBlur);
+	}
+}
